efedc8afa38129b9eaec0c410666536aff107040
Aaron Enyetu <aaronenyetu@gmail.com>
2024-03-20 11:24:39 -0700

    Successfully connected the frontend to the API to the database

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { Box, Button, ChakraProvider } from '@chakra-ui/react';
+import { Box, Button, ChakraProvider, Input } from '@chakra-ui/react';
 import axios from 'axios'
 
 function App() {
+  const [name, setName] = useState("test name 3");
+
+  const onChange = (event: any) => {
+    // console.log("EVENT: ", event.target.value)    
+    setName(event.target.value)
+  }
+
   const handleClick = async () => {
-    const response = axios.get('http://localhost:3025')
-    console.log("RESPONSE", response)
+    const response = await axios.post('http://localhost:3005/name', {
+      name,
+    })
+    console.log("RESPONSE", response.data)
   }
   return (
     <ChakraProvider>
-      <Box>Hi From APP</Box>
-      <Button colorScheme='purple' onClick={handleClick}>Button</Button>
+      <Box m={10} display="flex" gap={4}>
+        <Input onChange={onChange} placeholder="Type in a name..."/>
+        <Button colorScheme='purple' onClick={handleClick}>
+          Add Name
+        </Button>
+      </Box>
    </ChakraProvider>
   );
 }
